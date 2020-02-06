e2087a1a5a84fea392581533885727e8f1c21aaa
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-02-06 15:54:46 +0000

    end of lab commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("hello");
+dog1.addEventListener("click", image1_click);
+
+function image1_click(){
+  var img2 = document.getElementsByClassName('dog2');
+  //var img3 = document.getElementsByClassName('dog3');
+
+  var pos = 0;
+  var time = setInterval(frame, 3);
+
+  pos++;
+  img2.style.left = pos + 'px';
+}
