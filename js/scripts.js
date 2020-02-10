feec8d944945a2487e3a708b1bac5f68a47bb15c
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-02-10 18:39:49 +0000

    Updated placeholders. Logo moved to background. Moving elemnt temp rmv.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
-console.log("hello");
-dog1.addEventListener("click", image1_click);
+document.getElementsById("Location").addEventListener("click", image1_click);
 
 function image1_click(){
-<<<<<<< Updated upstream
-  var img2 = document.getElementsByClassName('dog2');
-  //var img3 = document.getElementsByClassName('dog3');
-
-  var pos = 0;
-  var time = setInterval(frame, 3);
-
-  pos++;
-  img2.style.left = pos + 'px';
+  document.getElementsById("team").classList.add('alternateStyle');
 }
-=======
-  //var img2 = document.getElementsByClassName('dog2');
-  //img2.className = "alternate";
-  document.getElementsById("dog2").setAttribute("class", "dog2-alternate");
->>>>>>> Stashed changes
