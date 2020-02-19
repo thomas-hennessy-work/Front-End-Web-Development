f00ab4fa8e2e483e26a9d3cbf72c50042b12d4d1
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-02-19 16:23:25 +0000

    Team image moves, with acompanying menu and descpription. Layout issue.

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,23 @@ const arena = document.getElementById('Location');
 const team = document.getElementById('team');
 const game = document.getElementById('game');
 
-//Appear on location click
+//ellements related to location
 const arena_more = document.getElementById('Location_more');
 const location_container = document.getElementById('Location_container')
 const location_discription = document.getElementById('Location_discription');
+
+//Ellements related to team
+const team_more = document.getElementById('Team_more');
+const team_container = document.getElementById('Team_container')
+const team_discription = document.getElementById('Team_discription');
 
 const arena_click = () => {
   //ask how to put the transitions for both ways in to css
   team.style.transition = "all 2s";
   game.style.transition = "all 2s";
   arena.classList.toggle("Location-click-location");
-  team.classList.toggle("Location-click-others");
-  game.classList.toggle("Location-click-others");
+  team.classList.toggle("click-right");
+  game.classList.toggle("click-right");
   arena_more.classList.toggle("Location-click-more_info");
   location_container.classList.toggle('Location-click-container');
   location_discription.classList.toggle('Location-click-discription');
@@ -24,7 +29,15 @@ const arena_click = () => {
 
 const team_click = () => {
   arena.style.transition = "all 2s";
+  team.style.transition = "all 2s"
   game.style.transition = "all 2s";
+  team.classList.toggle("Team-click-team");
+  arena.classList.toggle("click-left");
+  game.classList.toggle("click-right");
+  team_more.classList.toggle("Team-click-more_info");
+  team_container.classList.toggle("Team-click-container");
+  team_discription.classList.toggle('Team-click-discription');
+  location_discription.classList.toggle('Team-click-location-discription');
 }
 
 arena.addEventListener("click", arena_click);
