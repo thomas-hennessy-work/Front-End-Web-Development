a1acd9f151423cee2e909113d3bc9b41446a56dc
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-02-19 10:45:54 +0000

    Images no longer slide off together.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,8 @@ const arena_click = () => {
   team.style.transition = "all 2s";
   game.style.transition = "all 2s";
   arena.classList.toggle("Location-click-location");
-  team.classList.toggle("Location-click-team");
-  game.classList.toggle("Location-click-game");
+  team.classList.toggle("Location-click-others");
+  game.classList.toggle("Location-click-others");
 }
 
 arena.addEventListener("click", arena_click);
