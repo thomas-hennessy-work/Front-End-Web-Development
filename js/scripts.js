db47149cc1d50bc901d89ad2f58b01c8b05d9bd8
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-02-19 14:05:48 +0000

    Menu appears on click. Validated code. JS in strict mode.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
+"use strict";
+
 const arena = document.getElementById('Location');
 const team = document.getElementById('team');
 const game = document.getElementById('game');
+const arena_more = document.getElementById('Location_more');
+const location_container = document.getElementById('Location_container')
 
 const arena_click = () => {
   team.style.transition = "all 2s";
@@ -8,6 +12,8 @@ const arena_click = () => {
   arena.classList.toggle("Location-click-location");
   team.classList.toggle("Location-click-others");
   game.classList.toggle("Location-click-others");
+  arena_more.classList.toggle("Location-click-more_info");
+  location_container.classList.toggle('Location-click-container')
 }
 
 arena.addEventListener("click", arena_click);
