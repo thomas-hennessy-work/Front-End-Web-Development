ba6bd8eed55eb641598a71afc804ca4cc084bfa8
Tom Hennessy <P16176035@LEC-ADMIN.dmu.ac.uk>
2020-03-05 15:04:12 +0000

    Able to use multiple API calls simultaniously.

--- js/teams-scripts.js
+++ js/teams-scripts.js
@@ -1,24 +1,61 @@
-"use strict";
+// "use strict";
+//
+// var Request = new XMLHttpRequest();
+//
+// Request.open('GET', 'https://api.opendota.com/api/teams/2586976', true);
+// Request.onload = function(){
+//   var ogData = JSON.parse(this.response);
+//
+//   if(Request.status >= 200 && Request.status < 400){
+//     console.log(ogData.tag);
+//     console.log(ogData.team_id);
+//     console.log(ogData.wins);
+//     console.log(ogData.losses);
+//   }
+//   else{
+//     console.log('error');
+//   }
+//
+// }
+// Request.send();
 
-var Request = new XMLHttpRequest();
 
-Request.open('GET', 'https://api.opendota.com/api/teams', true);
-Request.onload = function(){
-  var ogData = JSON.parse(this.response);
 
-  if(Request.status >= 200 && Request.status < 400){
-    ogData.forEach(teamInfo => {
-      console.log(teamInfo.tag);
-      console.log(teamInfo.team_id);
+//Able to call multiple API's
+Promise.all([
+  fetch('https://api.opendota.com/api/teams/2586976'),
+  fetch('https://api.opendota.com/api/teams/2586976/players')
+])
+  .then(function (responses) {
+    return responses.map(function (response) {
+      return response.json();
     });
+  }).then(function (data) {
+    console.log(data);
+  }).catch(function (error) {
+    console.log(error);
+  });
 
-  }
-  else{
-    console.log('error');
-  }
 
-}
-Request.send();
+
+// Request.open('GET', 'https://api.opendota.com/api/teams/2586976/players', true);
+// Request.onload = function(){
+//   var ogPlayerData = JSON.parse(this.response);
+//
+//   if(Request.status >= 200 && Request.status < 400){
+//     ogPlayerData.forEach(player => {
+//       console.log(ogPlayerData.name);
+//       console.log(ogPlayerData.acount_id);
+//       console.log(ogPlayerData.games_played);
+//       console.log(ogPlayerData.wins);
+//     });
+//   }
+//   else{
+//     console.log('error');
+//   }
+//
+// }
+// Request.send();
 
 //OG ID = 2586976
 //PSG.LGD ID = 15
