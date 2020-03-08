594b69dbfbb9f3a881d01d0c73a1c673e8361590
Thomas Hennessy <p16176035@my365.dmu.ac.uk>
2020-03-08 22:08:45 +0000

    All ellements relating to team OG in table obtained from API.

--- js/teams-scripts.js
+++ js/teams-scripts.js
@@ -1,32 +1,13 @@
 "use strict";
 
-// Promise.all([
-//   fetch('https://api.opendota.com/api/teams/2586976'),
-//   fetch('https://api.opendota.com/api/teams/2586976/players')
-// ])
-//   .then (responses => {
-//     return responses.map(async function (response) {
-//       console.log(await response.json());
-//     });
-//   }).then (data => {
-//     let [teams,players] = data;
-//     console.log(teams);
-//     console.log(players);
-//
-//
-//     // activePlayers = players.filter(item => {return item.is_current_team_member == true});
-//     //
-//     // console.log(activePlayers);
-//   }).catch(error => {
-//     console.log(error);
-//   });
-
 fetch('https://api.opendota.com/api/teams/2586976').then(
   function(response_OG){
     return response_OG.json();
   }
-).then(function(teams){
-  console.log(teams);
+).then(function(OG_info){
+  console.log(OG_info);
+  document.getElementById("OG-name").innerHTML = OG_info.name;
+  document.getElementById("OG-logo").setAttribute("src", OG_info.logo_url);
 })
 
 fetch('https://api.opendota.com/api/teams/2586976/players').then(
@@ -36,43 +17,9 @@ fetch('https://api.opendota.com/api/teams/2586976/players').then(
 ).then(function(players){
     const activeOG = players.filter(players => players.is_current_team_member == true);
     console.log(activeOG);
+    document.getElementById("OG-member-0").innerHTML = activeOG[0].name;
+    document.getElementById("OG-member-1").innerHTML = activeOG[1].name;
+    document.getElementById("OG-member-2").innerHTML = activeOG[2].name;
+    document.getElementById("OG-member-3").innerHTML = activeOG[3].name;
+    document.getElementById("OG-member-4").innerHTML = activeOG[4].name;
 })
-
-// async function fetchData(url = '',){
-//   const response = await fetch(url, {
-//     method: 'get',
-//   });
-//   return await responce.json();
-// }
-//
-// fetch('https://api.opendota.com/api/teams/2586976')
-//   .then((team) => {
-//     console.log(team);
-//   });
-
-// Request.open('GET', 'https://api.opendota.com/api/teams/2586976/players', true);
-// Request.onload = function(){
-//   var ogPlayerData = JSON.parse(this.response);
-//
-//   if(Request.status >= 200 && Request.status < 400){
-//     ogPlayerData.forEach(player => {
-//       console.log(ogPlayerData.name);
-//       console.log(ogPlayerData.acount_id);
-//       console.log(ogPlayerData.games_played);
-//       console.log(ogPlayerData.wins);
-//     });
-//   }
-//   else{
-//     console.log('error');
-//   }
-//
-// }
-// Request.send();
-
-//OG ID = 2586976
-//PSG.LGD ID = 15
-//Team Secret ID = 1838315
-//TNC Predator = 2108395
-//Newbee ID = 6214538
-//Alliance ID = 111474
-//Team Liquid ID = 2163
